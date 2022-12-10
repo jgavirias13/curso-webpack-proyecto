c8ca514fb19032394e752637b0917648ab916d59
Juan Pablo Gaviria <juan.gaviria@gorillalogic.com>
2022-12-10 09:05:03 -0500

    Adding all the resources worked on class

---
+++ webpack.config.dev.js
@@ -0,0 +1,72 @@
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
+
+//Aqui en el objeto export es donde se agregan las configuraciones
+/** @type {import('webpack').Configuration} */
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[contenthash].js',
+    assetModuleFilename: 'assets/images/[hash][ext][query]',
+    clean: true
+  },
+  mode: 'development',
+  resolve: {
+    extensions: ['.js'],
+    alias: {
+      '@utils': path.resolve(__dirname, 'src/utils'),
+      '@styles': path.resolve(__dirname, 'src/styles'),
+      '@templates': path.resolve(__dirname, 'src/templates'),
+      '@images': path.resolve(__dirname, 'src/assets/images')
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }, 
+      {
+        test: /\.css|.styl$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'stylus-loader']
+      },
+      {
+        test: /\.png$/,
+        type: 'asset/resource'
+      },
+      {
+        test: /\.woff|.woff2$/,
+        type: 'asset/resource',
+        generator: {
+          filename: "assets/fonts/[name][ext]"
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      inject: true,
+      template: './public/index.html',
+      filename: './index.html'
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'assets/[name].[contenthash].css'
+    }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'src', 'assets/images'),
+          to: 'assets/images'
+        }
+      ]
+    }),
+    new Dotenv()
+  ]
+};
